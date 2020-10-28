620d51d75ec003bf3f2cd9d24d69c1eb0464a691
jhok2013 <jhok2013@gmail.com>
2020-10-27 18:36:33 -0600

    Added the employee data classes.

--- README.md
+++
@@ -1,3 +0,0 @@
-# GroupG
-
-A group repo so that we can practice using git and get a feel for a real development process.
---
+++ team_activity_7/main.py
@@ -0,0 +1,16 @@
+'''
+Authors: James Hough
+Date: 10/27/2020
+Version: 1.0.0
+'''
+
+from sys import path
+from pathlib import Path
+
+path.insert(0, str(Path('../..').resolve()))
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
