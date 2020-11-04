0ba92f73939562fc6f888bb7fe2154975fb97c25
jhok2013 <jhok2013@gmail.com>
2020-11-04 14:14:59 -0700

    Added seconds and minutes methods. Added unit tests for those methods.

--- team_activity_8/test/test_time.py
+++ team_activity_8/test/test_time.py
@@ -22,13 +22,321 @@ class test_time(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
     
+    
     def test_hours_setter(self):
         '''
 
         '''
-        hours: int = 22
+        # Arrange
+        expected_hours: int
+        normal_hours: int = 22
+        bad_small_hours: int = -1
+        bad_big_hours: int = 24
+        zero_hours: int = 0
+
+        # Act
+        ## Normal hours test
+        expected_hours = 22
+        self.time.hours = normal_hours
+        self.assertEqual(expected_hours, self.time.hours, msg='Ran test 1 successfully.')
+
+        ## Hours are too small test
+        expected_hours = 0
+        self.time.hours = bad_small_hours
+        self.assertEqual(expected_hours, self.time.hours, msg='Ran test 1 successfully.')
+
+        ## Hours are too big test
+        expected_hours = 23
+        self.time.hours = bad_big_hours
+        self.assertEqual(expected_hours, self.time.hours, msg='Ran test 1 successfully.')
+
+        ## Hours is 0
+        expected_hours = 0
+        self.time.hours = zero_hours
+        self.assertEqual(expected_hours, self.time.hours)
+    
+    
+    def test_minutes_normal(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_minutes: int = 45
+        expected_hours: int = 10
+        normal_minutes: int = 45
+        normal_hours: int = 10
+
+        # Act
+        self.time.hours = normal_hours
+        self.time.minutes = normal_minutes
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+
+    
+    def test_minutes_too_big(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_minutes: int = 59
+        expected_hours: int = 10
+        minutes: int = 60
+        hours: int = 10
+
+        # Act
         self.time.hours = hours
-        self.assertEqual(hours, self.time.hours, msg='Ran test 1 successfully.')
+        self.time.minutes = minutes
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+
+    
+    def test_minutes_too_small(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_minutes: int = 0
+        expected_hours: int = 10
+        minutes: int = -1
+        hours: int = 10
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+
+    
+    def test_minutes_zero(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_minutes: int = 0
+        expected_hours: int = 10
+        minutes: int = 0
+        hours: int = 10
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+
+    
+    def test_minutes_normal_zero_hour(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_minutes: int = 45
+        expected_hours: int = 0
+        minutes: int = 45
+        hours: int = 0
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+
+    
+    def test_seconds_normal(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_seconds: int = 30
+        expected_minutes: int = 45
+        expected_hours: int = 1
+        seconds: int = 30
+        minutes: int = 45
+        hours: int = 1
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+        self.time.seconds = seconds
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+        self.assertEqual(
+            first=expected_seconds,
+            second=self.time.seconds
+        )
+
+    
+    def test_seconds_big_seconds(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_seconds: int = 59
+        expected_minutes: int = 45
+        expected_hours: int = 1
+        seconds: int = 60
+        minutes: int = 45
+        hours: int = 1
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+        self.time.seconds = seconds
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+        self.assertEqual(
+            first=expected_seconds,
+            second=self.time.seconds
+        )
+
+    
+    def test_seconds_small_seconds(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_seconds: int = 0
+        expected_minutes: int = 45
+        expected_hours: int = 1
+        seconds: int = -1
+        minutes: int = 45
+        hours: int = 1
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+        self.time.seconds = seconds
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+        self.assertEqual(
+            first=expected_seconds,
+            second=self.time.seconds
+        )
+    
+    def test_seconds_zero(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_seconds: int = 0
+        expected_minutes: int = 45
+        expected_hours: int = 1
+        seconds: int = 0
+        minutes: int = 45
+        hours: int = 1
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+        self.time.seconds = seconds
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+        self.assertEqual(
+            first=expected_seconds,
+            second=self.time.seconds
+        )
+
+    def test_seconds_zero_minutes_zero_hours(self) -> None:
+        '''
+
+        '''
+        # Arrange
+        expected_seconds: int = 1
+        expected_minutes: int = 0
+        expected_hours: int = 0
+        seconds: int = 1
+        minutes: int = 0
+        hours: int = 0
+
+        # Act
+        self.time.hours = hours
+        self.time.minutes = minutes
+        self.time.seconds = seconds
+
+        # Act
+        self.assertEqual(
+            first=expected_hours,
+            second=self.time.hours
+        )
+        self.assertEqual(
+            first=expected_minutes,
+            second=self.time.minutes
+        )
+        self.assertEqual(
+            first=expected_seconds,
+            second=self.time.seconds
+        )
 
 if __name__ == '__main__':
     unittest.main()
--- team_activity_8/timeObject/Time.py
+++ team_activity_8/timeObject/Time.py
@@ -9,9 +9,6 @@ class Time(object):
     __period: str = 'AM'
     __hours_simple: int = 0
     __total_seconds: int = 0
-    __hours: int = 0
-    __minutes: int = 0
-    __seconds: int = 0
 
     def __init__(self) -> None:
         '''
@@ -24,7 +21,7 @@ class Time(object):
         '''
 
         '''
-        return self.__hours
+        return int(self.__total_seconds/pow(base=60, exp=2))
     
     @hours.setter
     def hours(self, hours: int) -> None:
@@ -37,10 +34,60 @@ class Time(object):
         elif hours < 0:
             hours = 0
         else:
-            hours = hours
+            # Do nothing
+            pass
 
-        # Remove self.__hours in seconds from self.__total_seconds
-        # and add hours in seconds
-        self.__total_seconds -= pow(base=60, exp=2) * self.__hours
+        # Get hours in seconds
+        self.__total_seconds -= pow(base=60, exp=2) * int(self.__total_seconds/pow(60,2)) \
+            if self.__total_seconds > 3599 else 0
         self.__total_seconds += pow(base=60, exp=2) * hours
-        self.__hours = hours
+    
+    @property
+    def minutes(self) -> int:
+        '''
+
+        '''
+        return int((self.__total_seconds - (self.hours * pow(60,2))) / 60)
+    
+    @minutes.setter
+    def minutes(self, minutes: int) -> None:
+        '''
+
+        '''
+        if minutes > 59:
+            minutes = 59
+        elif minutes < 0:
+            minutes = 0
+        else:
+            # Do nothing
+            pass
+
+        # Remove minutes in seconds from self.__total_seconds and then
+        # add minutes in seconds
+        self.__total_seconds -= int((self.__total_seconds - (self.hours * pow(60,2))) / 60) * 60
+        self.__total_seconds += minutes * 60
+    
+    @property
+    def seconds(self) -> int:
+        '''
+
+        '''
+        return (self.__total_seconds - (self.hours * pow(60,2))) % 60
+    
+    @seconds.setter
+    def seconds(self, seconds: int) -> None:
+        '''
+
+        '''
+        if seconds > 59:
+            seconds = 59
+        elif seconds < 0:
+            seconds = 0
+        else:
+            # Do nothing
+            pass
+
+        # Remove current seconds from self.__total_seconds and
+        # add seconds to self.__total_seconds
+        self.__total_seconds -= (self.__total_seconds - (self.hours * pow(60,2))) % 60
+        self.__total_seconds += seconds
